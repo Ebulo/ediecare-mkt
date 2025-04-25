9e35d6525e591bdb1639f5ec6e3cc6728653fd35
ebulo <bishant.nayak44@gmail.com>
2025-04-25 17:47:08 +0530

    First Commit

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "types"
+
+export const siteConfig: SiteConfig = {
+  name: "Eddiecare",
+  description:
+    "Eddiecare is a platform that connects patients with healthcare providers, offering a range of services including telemedicine, appointment scheduling, and health tracking.",
+  url: "https://tx.shadcn.com",
+  ogImage: "https://tx.shadcn.com/og.jpg",
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/taxonomy",
+  },
+}
